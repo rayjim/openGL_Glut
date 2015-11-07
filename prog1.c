bf9a6f30b5518fab8c06e63458f878da2a5ed426
rayjim <baoruihan@gmail.com>
2015-11-07 23:52:28 +0900

    finish 2D, begin 3D plot

--- prog1.c
+++ prog1.c
@@ -1,18 +1,25 @@
 #include <GL/glut.h>
+#include <stdlib.h>
+#define MAXPOINTS 100      /* 記憶する点の数　　 */
+GLint point[MAXPOINTS][2]; /* 座標を記憶する配列 */
+int pointnum = 0;          /* 記憶した座標の数　 */
+int rubberband = 0; /* ラバーバンドの消去 */
 
 void display(void)
 {
+  int i;
+
   glClear(GL_COLOR_BUFFER_BIT);
-  glBegin(GL_POLYGON);
-  glColor3d(1.0, 0.0, 0.0); /* 赤 */
-  glVertex2d(-0.9, -0.9);
-  glColor3d(0.0, 1.0, 0.0); /* 緑 */
-  glVertex2d(0.9, -0.9);
-  glColor3d(0.0, 0.0, 1.0); /* 青 */
-  glVertex2d(0.9, 0.9);
-  glColor3d(1.0, 1.0, 0.0); /* 黄 */
-  glVertex2d(-0.9, 0.9);
-  glEnd();
+
+  /* 記録したデータで線を描く */
+  if (pointnum > 1) {
+    glColor3d(0.0, 0.0, 0.0);
+    glBegin(GL_LINES);
+    for (i = 0; i < pointnum; ++i) {
+      glVertex2iv(point[i]);
+    }
+    glEnd();
+  };
   glFlush();
 }
 void resize(int w, int h)
@@ -20,11 +27,90 @@ void resize(int w, int h)
   /* ウィンドウ全体をビューポートにする */
   glViewport(0, 0, w, h);
 
-  /* 変換行列の初期化 */
+  /* 変換行列の初期化 */  
   glLoadIdentity();
 
-  /* スクリーン上の表示領域をビューポートの大きさに比例させる */
-  glOrtho(-w / 200.0, w / 200.0, -h / 200.0, h / 200.0, -1.0, 1.0);
+  /* スクリーン上の座標系をマウスの座標系に一致させる */
+  glOrtho(-0.5, (GLdouble)w - 0.5, (GLdouble)h - 0.5, -0.5, -1.0, 1.0);
+}
+
+void mouse(int button, int state, int x, int y)
+{
+  /* 削除 */
+
+  switch (button) {
+  case GLUT_LEFT_BUTTON:
+    /* ボタンを操作した位置を記録する */
+    point[pointnum][0] = x;
+    point[pointnum][1] = y;
+    if (state == GLUT_UP) {
+      /* ボタンを押した位置から離した位置まで線を引く */
+      glColor3d(0.0, 0.0, 0.0);
+      glBegin(GL_LINES);
+      glVertex2iv(point[pointnum - 1]); /* 一つ前は押した位置　 */
+      glVertex2iv(point[pointnum]);     /* 今の位置は離した位置 */
+      glEnd();
+      glFlush();
+      /* 始点ではラバーバンドを描いていないので消さない */
+      rubberband = 0;
+    }    
+    else {
+      /* 削除 */
+    }
+    if (pointnum < MAXPOINTS - 1) ++pointnum;
+    break;
+  case GLUT_MIDDLE_BUTTON:
+    break;
+  case GLUT_RIGHT_BUTTON:
+    break;
+  default:
+    break;
+  }
+}
+
+void motion(int x, int y)
+{
+  static GLint savepoint[2]; /* 以前のラバーバンドの端点 */
+
+  /* 論理演算機能 ON */
+  glEnable(GL_COLOR_LOGIC_OP);
+  glLogicOp(GL_INVERT);
+
+  glBegin(GL_LINES);
+  if (rubberband) {
+    /* 以前のラバーバンドを消す */
+    glVertex2iv(point[pointnum - 1]);
+    glVertex2iv(savepoint);
+  }
+  /* 新しいラバーバンドを描く */
+  glVertex2iv(point[pointnum - 1]);
+  glVertex2i(x, y);
+  glEnd();
+
+  glFlush();
+
+  /* 論理演算機能 OFF */
+  glLogicOp(GL_COPY);
+  glDisable(GL_COLOR_LOGIC_OP);
+
+  /* 今描いたラバーバンドの端点を保存 */
+  savepoint[0] = x;
+  savepoint[1] = y;
+ 
+  /* 今描いたラバーバンドは次のタイミングで消す */
+  rubberband = 1;
+}
+
+void keyboard(unsigned char key, int x, int y)
+{
+  switch (key) {
+  case 'q':
+  case 'Q':
+  case '\033':  /* '\033' は ESC の ASCII コード */
+    exit(0);
+  default:
+    break;
+  }
 }
 
 void init(void)
@@ -42,6 +128,9 @@ int main(int argc, char *argv[])
   glutCreateWindow(argv[0]);
   glutDisplayFunc(display);
   glutReshapeFunc(resize);
+  glutMouseFunc(mouse);
+  glutMotionFunc(motion);
+  glutKeyboardFunc(keyboard);
   init();
   glutMainLoop();
   return 0;
